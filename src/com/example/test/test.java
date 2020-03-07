ee36c4e3f922308c57e8322f09f463be269e2417
ggez <gegezy33@gmail.com>
2020-03-07 11:08:46 +0700

    Initial commit

---
+++ src/com/example/test/test.java
@@ -0,0 +1,16 @@
+package com.example.test;
+
+public class test {
+    public static void main(String[] args) {
+        String input = "Geeks for Geeks";
+
+        StringBuilder input1 = new StringBuilder();
+        // append a string into StringBuilder input1
+        input1.append(input);
+        // reverse StringBuilder input1
+        input1 = input1.reverse();
+
+        // print reversed String
+        System.out.println(input1);
+    }
+}
