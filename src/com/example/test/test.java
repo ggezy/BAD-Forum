9c37b9ed4431f1227aba7989bb5a46b8deef6434
ggez <gegezy33@gmail.com>
2020-03-22 11:47:22 +0700

    update terbaru

--- src/com/example/test/test.java
+++ src/com/example/test/test.java
@@ -1,38 +1,77 @@
 package com.example.test;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class test {
 
-    public static String reverse(String str){
-        return new StringBuilder(str).reverse().toString();
-    }
     public static void main(String[] args) {
-        String string1 = "KOPASUS";
-        String string2 = "JAMAICA";
+        //mendeklarasi scanner class ke dalam variable input
+        Scanner input = new Scanner (System.in);
 
-        //melakukan reverse untuk kata yang pertama
-        string1 = reverse(string1);
-        StringBuilder sb1 = new StringBuilder();
+        System.out.println("masukkan jumlah data yang akan Anda input : ");
 
-        //membuat string menjadi char dan menambahkan koma
-        for (char c : string1.toCharArray()) {
-            sb1.append(c).append(",");
+        int jumlahData=input.nextInt();
+        //mendefinisikan array sesuai dengan jumlah data user
+        int arr[]=new int[jumlahData];
+
+        System.out.println("masukkan angka : ");
+
+        for(int i=0;i<jumlahData;i++){ //memasukkan angka ke dalam array
+            arr[i]=input.nextInt();
         }
-        //menghapus koma terakhir
-        if( sb1.length() > 0 )
-            sb1.deleteCharAt( sb1.length() - 1 );
-        System.out.println(sb1);
 
-        //melakukan reverse untuk kata yang kedua
-        string2 = reverse(string2);
-        StringBuilder sb2 = new StringBuilder();
+        //memanggil method untuk mencari value terkecil
+        int min = angkaTerkecil(arr);
+        System.out.println("Nilai minimum = " + min);
 
-        //membuat string menjadi char dan menambahkan koma
-        for (char c2 : string2.toCharArray()) {
-            sb2.append(c2).append(",");
+        //memanggil method untuk mencari value terbesar
+        int max = angkaTerbesar(arr);
+        System.out.println("Nilai maksimum = " + max);
+
+        //memanggil method untuk mencari nilai median
+        int median = nilaiMedian(arr);
+        System.out.println("Nilai median = " + median);
+    }
+
+    //method untuk mencari value terbesar
+    public static int angkaTerbesar(int[] inputArray) {
+
+        //mendefinisikan value terbesar mulai dari index 0
+        int max = inputArray[0];
+        //melakukan komparasi setiap value yang ada di array
+        for (int i = 1; i < inputArray.length; i++)
+            if (inputArray[i] > max)
+                max = inputArray[i];
+        return max;
+    }
+
+    //method untuk mencari value terkecil
+    public static int angkaTerkecil(int[] inputArray) {
+
+        //mendefinisikan value terkecil mulai dari index 0
+        int min = inputArray[0];
+        //melakukan komparasi setiap value yang ada di array
+        for (int i = 1; i < inputArray.length; i++)
+            if (inputArray[i] < min)
+                min = inputArray[i];
+        return min;
+    }
+
+    //method untuk mencari nilai median
+    public static int nilaiMedian(int[] inputArray){
+        //sortir array untuk melakukan perhitungan median
+        Arrays.sort(inputArray);
+
+        int median;
+        int totalElements = inputArray.length;
+
+        //melakukan pengecekan bila jumlah array adalah genap atau ganjil
+        if (totalElements % 2 == 0) { //bila genap maka melakukan kalkulasi value yang berada di index tengah array
+            median = (inputArray[inputArray.length/2] + inputArray[inputArray.length/2 - 1])/2;
+        } else { //bila ganjil maka mengambil value yang berada di index tengah array
+            median = inputArray[inputArray.length/2];
         }
-        //menghapus koma terakhir
-        if( sb2.length() > 0 )
-            sb2.deleteCharAt( sb2.length() - 1 );
-        System.out.println(sb2);
+        return median;
     }
 }
